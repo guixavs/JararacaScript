ce1b3088b5d7b771f71b09af1d4750f9315264e4
Guilherme Xavier Souza <guilherme.xavier.souza.0@gmail.com>
2025-03-29 22:16:02 -0300

    Update JararacaScript.py
    Adição de porcentagem e tempo de execução

--- JararacaScript.py
+++ JararacaScript.py
@@ -1,21 +1,20 @@
 from bs4 import BeautifulSoup as bs
+from os import replace
 from requests import get
 import pandas as pd
-from os import replace
+import time
 
 
-def dados_orientador(url, id=0):
+def dados_orientador(url: str) -> list:
     """Função que retorna os dados do orientador"""
-    page = get(url)
-    soup = bs(page.text, "html.parser")
-    nome = soup.find("h2", attrs={"itemprop": "name"}).text
+    soup = bs(get(url).text, "html.parser")
     tabela = soup.find("table", attrs={"id": "table-profile-ascendants"})
+    valores: list = [0, soup.find("h2", attrs={"itemprop": "name"}).text]
+    anos: str = ""
     linhas = tabela.findAll("tr")
-    valores = [id, nome]
-    anos = ""
     for l in linhas[1:]:
         dado = l.findAll("td")
-        linha = [tr.text for tr in dado]
+        linha: list[str] = [tr.text for tr in dado]
         linha[1] = limpa_nome(linha[1])
         linha[2] = limpa_nome(linha[2])
         anos += linha[2]
@@ -36,7 +35,7 @@ def dados_orientador(url, id=0):
     return valores
 
 
-def criar_arquivos(url):
+def criar_arquivos(url: str) -> str:
     """Função que cria o arquivo csv com os cabeçalhos"""
     nodes = pd.DataFrame(
         columns=["Id", "Nome", "Tipo", "Ano", "Ds", "IG", "Fc", "Ft", "G", "R", "Pr"]
@@ -52,16 +51,22 @@ def criar_arquivos(url):
     return nome
 
 
-def nomear_arquivos(nome, nodes="nodes.csv", edges="edges.csv", links="links.txt"):
+def nomear_arquivos(
+    nome: str,
+    nodes: str = "nodes.csv",
+    edges: str = "edges.csv",
+    links: str = "links.txt",
+) -> None:
+    """Função que renomeia os arquivos gerados"""
     replace(nodes, f"{nome}_nodes.csv")
     replace(edges, f"{nome}_edges.csv")
     replace(links, f"{nome}_links.txt")
 
 
-def criar_tabela(url, arquivo="nodes.csv", id=0):
+def criar_tabela(url: str, arquivo: str = "nodes.csv", id: int = 0):
     """Função que cria uma tabela com os dados dos orientados de um professor"""
-    page = get(url)
-    soup = bs(page.text, "html.parser")
+    soup = bs(get(url).text, "html.parser")
+    print(f"\nBaixando dados de {soup.find("h2", attrs={"itemprop": "name"}).text}")
     tabela = soup.find("table", attrs={"id": "table-profile-descendants"})
     plan = pd.DataFrame(
         columns=["Id", "Nome", "Tipo", "Ano", "Ds", "IG", "Fc", "Ft", "G", "R", "Pr"]
@@ -91,22 +96,28 @@ def criar_tabela(url, arquivo="nodes.csv", id=0):
                 link
             ):  # Verifica se o orientado tem orientados
                 links.append([link, int(linha[0])])
-    print(f"Baixando dados de: {linha[1]}")
     plan.to_csv(arquivo, mode="a", header=False, index=False)
+    print(f"O processo está {porcentagem():.2f}% completo")
     define_edges(plan, id)  # Chama a função para definir as arestas
-    """Ver a ordenação"""
     for link in txt:
         adicionar_cache(link)
     for link in links:
         criar_tabela(link[0], "nodes.csv", link[1])  # Recursão
 
+def porcentagem(arquivo: str = "nodes.csv") -> float:
+    """Função que calcula a porcentagem de orientados"""
+    plan = pd.read_csv(arquivo)
+    return (len(plan) - 1) / int(plan["Ds"][0]) * 100
+   
 
-def adicionar_cache(link):
+def adicionar_cache(link: str) -> None:
+    """Função que adiciona o link no arquivo links.txt"""
     with open("links.txt", "a") as myfile:
         myfile.write("\n" + link)
 
 
-def duplicado(link):
+def duplicado(link: str) -> bool:
+    """Função que verifica se o link já existe no arquivo links.txt"""
     with open("links.txt") as file:
         lines = [line.rstrip() for line in file]
     if link in lines:
@@ -115,14 +126,14 @@ def duplicado(link):
         return False
 
 
-def limpa_nome(nome):
+def limpa_nome(nome: str) -> str:
     """Função que limpa o nome dos orientados"""
     nome = nome.replace("\n", "")
     nome = nome.replace("  ", "")
     return nome
 
 
-def limpa_ano(str):
+def limpa_ano(str: str) -> tuple:
     """Função que limpa o ano dos orientados"""
     if len(str) > 5:  # Tratamento para o caso de ter dois anos
         ano1 = int(str[1:5])
@@ -139,7 +150,7 @@ def limpa_ano(str):
     return tipo, ano
 
 
-def define_edges(plan, id):
+def define_edges(plan, id: int) -> None:
     """Função que define as arestas"""
     tabela = pd.DataFrame(columns=["Source", "Target"])
     final = len(pd.read_csv("edges.csv"))
@@ -148,14 +159,14 @@ def define_edges(plan, id):
         tabela.to_csv("edges.csv", mode="a", header=False, index=False)
 
 
-def limpa_arquivo(arquivo="nodes.csv"):
+def limpa_arquivo(arquivo: str = "nodes.csv"):
     """Função que limpa o arquivo para ficar apenas com os dados necessários"""
     plan = pd.read_csv(arquivo)
     plan.drop(["IG", "Fc", "Ft", "G", "R", "Pr"], axis=1, inplace=True)
     plan.to_csv(arquivo, index=False)
 
 
-def add_ano_ord(arquivo="nodes.csv"):
+def add_ano_ord(arquivo: str = "nodes.csv"):
     plan = pd.read_csv(arquivo)
     ano_i = int(plan["Ano"][0])
     ano_ord = []
@@ -165,24 +176,24 @@ def add_ano_ord(arquivo="nodes.csv"):
     plan.to_csv(arquivo, index=False)
 
 
-def main():
+def main() -> None:
     print("\nBem-vindo ao JararacaScript!")
     print(
         "\nEste programa irá baixar os dados de orientação de um professor por meio da Plataforma Acácia"
     )
     url = input("\nPor favor, insira a URL do professor que deseja baixar os dados: ")
-    """Exemplos de URL para teste"""
+    # Exemplos de URL para teste
     # url = "https://plataforma-acacia.org/profile/tereza-maria-de-azevedo-pires-serio/"
     # url = "https://plataforma-acacia.org/profile/isaias-pessotti/"
     # url = "https://plataforma-acacia.org/profile/silvia-tatiana-maurer-lane/"
     # url = "https://plataforma-acacia.org/profile/carolina-martuscelli-bori/"
     # url = "https://plataforma-acacia.org/profile/maria-do-carmo-guedes/"
     nome = criar_arquivos(url)
-    print(f"\nBaixando dados de orientação de: {nome}\n")
+    print(f"\nOrientador: {nome}")
     criar_tabela(url)
     limpa_arquivo()  # Limpa o arquivo para ficar apenas com os dados necessários
-    add_ano_ord()
-    nomear_arquivos(nome)
+    add_ano_ord()  # Adiciona a coluna Ano_Ord
+    nomear_arquivos(nome)  # Renomeia os arquivos
     print("\nOs dados foram baixados com sucesso!")
     print("\nOs arquivos gerados foram:")
     print(f"{nome}_nodes.csv")
@@ -190,5 +201,6 @@ def main():
     print(f"{nome}_links.txt")
     print("\nObrigado por usar o JararacaScript!")
 
-
+tempo = time.time()
 main()
+print(f"\nTempo de execução: {time.time() - tempo:.2f} segundos")
